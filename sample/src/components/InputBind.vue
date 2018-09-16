02ca77aee0c0d2f2380d290c6c146a4891a25972
kflange <zknsftk1msrk@gmail.com>
2018-09-16 23:39:28 +0900

    set bind

---
+++ sample/src/components/InputBind.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="input-bind">
+  <input type="text" v-model="message">
+  <p>Message is: {{ message }}</p>
+  <p>sample</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component({})
+export default class InputBind extends Vue {
+  private message: string;
+
+  constructor() {
+    super();
+    this.message = 'abc';
+  }
+}
+</script>
--- sample/src/views/A.vue
+++ sample/src/views/A.vue
@@ -1,3 +1,19 @@
 <template>
+  <div>
   <h1>A</h1>
+  <InputBind/>
+  </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import InputBind from '@/components/InputBind.vue';
+
+@Component({
+  components: {
+    InputBind,
+  },
+})
+
+export default class A extends Vue {}
+</script>
